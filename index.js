1aa164eba000d48afbe24edb4a96adea66be9e3e
fiachmchh <fiachmchh@gmail.com>
2021-01-19 01:20:09 +0000

    answer working and being put into readme

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var inquirer = require('inquirer');
-// const fs = require('fs');
+const fs = require('fs');
 
 inquirer
   .prompt([
@@ -13,4 +13,23 @@ inquirer
   .then(answers => {
     // Use user feedback for... whatever!!
     console.log(answers)
+
+
+    var newReadMe = `
+    
+    User name ${answers.user_name}
+
+`
+
+fs.writeFile('newReadMe.md', newReadMe, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+});
+console.log('New Readme!!', newReadMe)
+
+
+
+
   })
+
+
