420e0db5a17e8b22e8a7f35ef986f6f95cf980b7
fiachmchh <fiachmchh@gmail.com>
2021-01-19 13:43:40 +0000

    questions inserted

--- index.js
+++ index.js
@@ -1,25 +1,146 @@
+// TODO: Include packages needed for this application
+
 var inquirer = require('inquirer');
 const fs = require('fs');
 
+
+// TODO: Create an array of questions for user input
 inquirer
   .prompt([
+    {
+        name: "title",
+        type: "input",
+        message: "What is the project title?",
+    },
     {
       name: "user_name",
       type: "input",
       message: "What is your name?",
     },
+    {
+        name: "description1",
+        type: "input",
+        message: "What were your motivations and what problems did you solve by building this project?",
+    },
+    {
+        name: "description2",
+        type: "input",
+        message: "What did you learn by building this project?",
+    },
+    {
+        name: "description3",
+        type: "input",
+        message: "What makes this project stand out from others like it?",
+    },
+    {
+        name: "deployed",
+        type: "input",
+        message: "Please provide a link to your deployed application here.",
+    },
+    {
+        name: "installation",
+        type: "input",
+        message: "Please provide a step by step description of how to install the project.",
+    },
+    {
+        name: "usage",
+        type: "input",
+        message: "Please provide instructions on how to use the app.",
+    },
+    {
+        name: "credits",
+        type: "input",
+        message: "Please list any collabortors on this project",
+    },
+    {
+        name: "license",
+        type: "input",
+        message: "You can choose the appropriate license by following the link on the generated ReadMe. Press return to continue.",
+    },
+    {
+        name: "badges",
+        type: "input",
+        message: "Include any links to badges for your ReadMe here.",
+    },
+    {
+        name: "features",
+        type: "input",
+        message: "Please list any features that your project has here.",
+    },
+    {
+        name: "contributing",
+        type: "input",
+        message: "You can follow the link for instructions on contributing. Press return to continue",
+    },
+    {
+        name: "tests",
+        type: "input",
+        message: "Insert any tests for your application here and provide examples on how to run them.",
+    },
     
   ])
+
+  // TODO: Create a function to write README file
+
   .then(answers => {
     // Use user feedback for... whatever!!
     console.log(answers)
 
 
     var newReadMe = `
+
+    # Title : ${answers.title}
+    # Author : ${answers.user_name}
+
+    ## Description :
+    ${answers.description1}
+
+    ${answers.description2}
+
+    ${answers.description3}
+
+    ${answers.deployed}
+
+    ## Table of Contents :
     
-    User name ${answers.user_name}
+    - Installation 
+    - Usage
+    - Credits
+    - License
+
+    - Badges
+    - Features
+    - Contributing
+    - Tests
+
+    ## Installation
+    ${answers.installation}
+
+    ## Usage
+    ${answers.usage}
+
+    ## Credits
+    ${answers.credits}
+
+    ## License
+    You can choose the appropriate license by following the link below:
+    (https://choosealicense.com/)
+
+    ## Badges
+    ${answers.badges}
+
+    ## Features
+    ${answers.features}
+
+    ## Contributing
+    You can follow the link below for instructions on contributing:
+    (https://www.contributor-covenant.org/)
+
+    ## Tests
+    ${answers.tests}
 
 `
+// TODO: Create a function to initialize app
 
 fs.writeFile('newReadMe.md', newReadMe, function (err) {
     if (err) throw err;
@@ -28,8 +149,8 @@ fs.writeFile('newReadMe.md', newReadMe, function (err) {
 console.log('New Readme!!', newReadMe)
 
 
+})
 
-
-  })
+// Function call to initialize app
 
 
